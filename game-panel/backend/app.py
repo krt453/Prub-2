3146d72f4f83bb8fe577f798830f96ba0d38e43b
krt453 <94526880+krt453@users.noreply.github.com>
2025-07-02 13:41:17 +0200

    fix db creation context

--- game-panel/backend/app.py
+++ game-panel/backend/app.py
@@ -53,5 +53,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000)
